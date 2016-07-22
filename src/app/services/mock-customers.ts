36d4ecbece6947ca09792803cf60fd7caec76323
sanketsw <sanketsw@au1.ibm.com>
2016-07-22 11:25:07 +1000

    Customer mocked data

--- src/app/services/mock-customers.ts
+++ src/app/services/mock-customers.ts
@@ -1,7 +1,7 @@
 import { Customer } from '../model/customer';
 
 export const CUSTOMERS: Customer[] = [
-  { id: '11', name: 'Mr. Nice', revenue: 45829, expiry: 15 },
-  { id: '12', name: 'Narco', revenue: 75960, expiry: 45 },
-  { id: '13', name: 'Bombasto', revenue: 64923, expiry: 150 }
+  { id: '11', name: 'Mr. Nice', revenue: 45829, expiry: 15, difference: -1467 },
+  { id: '12', name: 'Narco', revenue: 75960, expiry: 45, difference: -1467 },
+  { id: '13', name: 'Bombasto', revenue: 64923, expiry: 150, difference: -1467 }
 ];

c0bf52d6388bea66875cf0a0423733f4c8149daf
Tor Nilsen <torbjorn.nilsen@gmail.com>
2016-07-22 21:05:32 +1000

    Fix lint errors.

--- src/app/product-planner/product-planner.component.ts
+++ src/app/product-planner/product-planner.component.ts
@@ -1,14 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {Button, InputText, Password, Panel, DataList} from 'primeng/primeng';
-import {Router} from '@angular/router';
-import {Accordion} from 'primeng/primeng';
-import {AccordionTab} from 'primeng/primeng';
-
+import { Component, OnInit } from '@angular/core';
+import { InputText, Password, Panel, DataList } from 'primeng/primeng';
+import { Router } from '@angular/router';
+import { Accordion } from 'primeng/primeng';
+import { AccordionTab } from 'primeng/primeng';
 import { ProductService } from '../services/product.service';
 import { CategoryService } from '../services/category.service';
-import { Category } from '../model/product';
-import { Plan } from '../model/plan';
-
+import { Category } from '../model/category';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'as-product-planner',
@@ -16,22 +14,17 @@ import { Plan } from '../model/plan';
   styleUrls: [
     'app/product-planner/product-planner.css'
   ],
-  directives: [Button, InputText, Password, Panel, DataList, Accordion, AccordionTab],
+  directives: [InputText, Password, Panel, DataList, Accordion, AccordionTab],
   providers: [ProductService, CategoryService]
 })
 
 export class ProductPlannerComponent implements OnInit {
 
   products: Product[];
-
   categories: Category[];
-
   selectedProduct: Product;
-
   selectedCategory: Category;
-  
   plannedPrice: number;
-  
   initialPrice: number;
 
   constructor(private router: Router, private productService: ProductService, private categoryService: CategoryService) {
@@ -41,28 +34,28 @@ export class ProductPlannerComponent implements OnInit {
   recalculate() {
     this.calculate(this.products);
   }
-  
+
   calculate(products) {
     this.plannedPrice = 0;
-    for(let currentProduct of products) {
+    for (let currentProduct of products) {
       this.plannedPrice += (currentProduct.price * currentProduct.count);
     }
   }
-  
+
   calculateInitialPrice(products) {
     this.initialPrice = 0;
-    for(let currentProduct of products) {
+    for (let currentProduct of products) {
       this.initialPrice += (currentProduct.price * currentProduct.count);
     }
   }
-  
+
   ngOnInit() {
     this.productService.getProductsInCurrentPlan().then(products => this.products = products);
     this.categoryService.getCategories().then(categories => this.categories = categories);
-	this.productService.getProductsInCurrentPlan().then(products => this.calculate(products));
+    this.productService.getProductsInCurrentPlan().then(products => this.calculate(products));
     this.productService.getProductsInCurrentPlan().then(products => this.calculateInitialPrice(products));
   }
-  
+
   selectProduct(product: Product) {
     this.selectedProduct = product;
   }
@@ -75,17 +68,9 @@ export class ProductPlannerComponent implements OnInit {
     product.count ++;
     this.recalculate();
   }
-  
-  plannedPrice() {
-    return this.initialPrice;
-  }
 
-  initialPrice() {
-    return this.initialPrice;
-  }
-  
   deSelect(product: Product) {
-    if(product.count > 0) {
+    if (product.count > 0) {
       product.count --;
     }
     this.recalculate();
--- src/app/services/mock-products.ts
+++ src/app/services/mock-products.ts
@@ -1,9 +1,9 @@
 import { Product } from '../model/product';
 
 export const PRODUCTS: Product[] = [
-    { id: '51', name: 'Product 1', categoryId: '', count: '', description: 'Product 1 description', price: 1234  },
-    { id: '52', name: 'Product 2', categoryId: '', count: '', description: 'Product 1 description', price: 2345  },
-    { id: '53', name: 'Product 3', categoryId: '', count: '', description: 'Product 1 description', price: 3456  }
+    { id: '51', name: 'Product 1', categoryId: '', count: 0, description: 'Product 1 description', price: 1234  },
+    { id: '52', name: 'Product 2', categoryId: '', count: 0, description: 'Product 1 description', price: 2345  },
+    { id: '53', name: 'Product 3', categoryId: '', count: 0, description: 'Product 1 description', price: 3456  }
 ];
 
 export const PRODUCTS_IN_PLAN: Product[] = [

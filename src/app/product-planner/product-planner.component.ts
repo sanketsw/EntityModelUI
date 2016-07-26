fcca3649b830bb16a69a523980d66576934f0de4
sanketsw <sanketsw@au1.ibm.com>
2016-07-26 15:01:09 +1000

    multi select value type

--- src/app/product-planner/product-planner.component.ts
+++ src/app/product-planner/product-planner.component.ts
@@ -29,7 +29,7 @@ export class ProductPlannerComponent implements OnInit {
   plannedPrice: number;
   initialPrice: number;
   promotions: SelectItem[];
-  selectedPromotions: string;
+  selectedPromotions: number[];
   msgs: Message[] = [];
 
 
